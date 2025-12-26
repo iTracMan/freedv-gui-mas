8fbfb6af4f4d16b8c5a6ac8d2d78923b940757c9
Mark <g1inu@btinternet.com>
2025-12-26 21:07:49 +0000

    Rename button variable from m_GT2 to m_gt2

--- src/topFrame.h
+++ src/topFrame.h
@@ -133,7 +133,7 @@ class TopFrame : public wxFrame
         wxToggleButton      *m_audioRecord;
         
         wxButton*     m_logQSO;
-        wxButton*     m_GT2;
+        wxButton*     m_gt2;
 
         wxRadioButton *m_rbRADE;
         wxRadioButton *m_rb700d;
